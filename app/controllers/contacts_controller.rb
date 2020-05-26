45826add44c6a7cc4e0f06f322a138afe52d8b4b
guillermo varela <guillovarela16@gmail.com>
2020-05-26 19:02:30 -0300

    Finish the ajax thing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,10 +9,10 @@ class ContactsController < ApplicationController
     respond_to do |format|
       if @contact.deliver
         format.html { redirect_to 'pages/home' }
-        format.js { flash[:alert] = "Gracias por su mensaje, lo contactaremos pronto" }
+        format.js { flash[:alert] = "Thanks for your message we will contact soon" }
       else
         format.html { render 'index' }
-        format.js   { flash[:notice] = "Thank you for" }
+        format.js
       end
     end
   end
