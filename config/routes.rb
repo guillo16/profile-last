8c4cc839077e0e19b3f1ec40a21c4bd6b739c913
guillermo varela <guillovarela16@gmail.com>
2020-05-26 16:22:38 -0300

    Contact in progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'contacts/new'
   get 'contact/new'
   root to: 'pages#home'
+
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
